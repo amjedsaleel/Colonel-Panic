8c4b9c2093fb61224af281d2566341e218b8db9d
Amjed Saleel K <amjedsaleel@gmail.com>
2021-02-14 11:31:10 +0530

    Company and Bank account table created for startups

--- startup/admin.py
+++ startup/admin.py
@@ -1,3 +1,10 @@
+# Django
 from django.contrib import admin
 
+# local django
+from . models import Company, BankAccount
+
 # Register your models here.
+
+admin.site.register(Company)
+admin.site.register(BankAccount)
--- startup/models.py
+++ startup/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+user = get_user_model()
+
+
+class Company(models.Model):
+    owner = models.OneToOneField(to=user, on_delete=models.CASCADE)
+    name = models.CharField(max_length=150)
+    subtitle = models.CharField(max_length=50)
+    description = models.TextField(max_length=255)
+    location = models.CharField(max_length=255)
+    employees = models.PositiveIntegerField()
+    theme = models.TextField(max_length=255, help_text='IT/Food/Art etc')
+    revenue = models.PositiveIntegerField()
+    expected_revenue = models.PositiveIntegerField()
+
+    class Meta:
+        verbose_name ='Companie'
+
+    def __str__(self):
+        return self.name
+
+
+class BankAccount(models.Model):
+    owner = models.OneToOneField(to=Company, on_delete=models.CASCADE)
+    account_holder = models.CharField(max_length=155, help_text='Name of the bank account owner')
+    account_number = models.CharField(max_length=50)
+    ifsc_code = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.account_number
+
